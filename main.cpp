5807f9ce1a7c13cdf198f10f16744c984fabb4ad
Pome <114458716+pomegranata@users.noreply.github.com>
2022-12-14 23:12:02 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main()
 {
     string ps;
-    int jam, mnt;
+    int jam, mnt, durasi, bayar, jam_ht, menit_ht, durasi_ht, jam_bts, menit_bts;
+    char paket;
+
+
     cout << "------------------------------------------------------------------------" << endl;
     cout << "   Paket  |   PS 3    |   PS 4    |   PS 5    |   Durasi   |   Bonus   |" << endl;
     cout << "------------------------------------------------------------------------" << endl;
@@ -16,40 +19,102 @@ int main()
     cout << "   3.     |   15.000  |   20.000  |   27.000  |  180 menit |  30 menit |" << endl;
     cout << "------------------------------------------------------------------------" << endl;
 
-    cout << "Jam mulai bermain: ";
+    cout <<"\n========================================================================" << endl;
+
+    cout << "\nJam mulai bermain: ";
     cin >> jam;
 
     cout << "Menit mulai bermain: ";
     cin >> mnt;
 
-    cout << "Jenis PS? (capslock + spasi): ";
+    cout << "Jenis PS? (capslock tanpa spasi): ";
     cin >> ps;
 
     cout << "Pilih paket: ";
     cin >> paket;
 
-    if (ps == "PS 3")
+    cout <<"\n========================================================================" << endl;
+
+    if (ps == "PS3")
     {
-        if (paket == 1)
+        if (paket == '1')
         {
             durasi = 60;
             bayar = 5000;
         }
-        else if (paket == 2 )
+        else if (paket == '2' )
         {
             durasi = 140;
-            bayar
+            bayar = 10000;
+        }
+        else if (paket == '3')
+        {
+            durasi = 210;
+            bayar = 15000;
         }
         else
         {
-            durasi = 210;
+            cout << "Paket tidak tersedia" << endl;
         }
     }
-    else if (ps == "PS 4")
+    else if (ps == "PS4")
     {
-        if paket
+        if (paket == '1')
+        {
+            durasi = 60;
+            bayar = 7000;
+        }
+        else if (paket == '2' )
+        {
+            durasi = 140;
+            bayar = 14000;
+        }
+        else if (paket == '3')
+        {
+            durasi = 210;
+            bayar = 20000;
+        }
+        else
+        {
+            cout << "Paket tidak tersedia" << endl;
+        }
     }
-    //cout << "Batas bermain " << jam_bts << " : " << menit_bts;//
+    else if (ps == "PS5")
+    {
+        if (paket == '1')
+        {
+            durasi = 60;
+            bayar = 5000;
+        }
+        else if (paket == '2' )
+        {
+            durasi = 140;
+            bayar = 10000;
+        }
+        else if (paket == '3')
+        {
+            durasi = 210;
+            bayar = 15000;
+        }
+        else
+        {
+            cout << "Paket tidak tersedia" << endl;
+        }
+    }
+    else
+    {
+        cout << "PS tidak tersedia" << endl;
+    }
+
+    jam_ht = jam * 60;
+    menit_ht = mnt + jam_ht;
+    durasi_ht = menit_ht + durasi;
+
+    jam_bts = durasi_ht / 60;
+    menit_bts = durasi_ht % 60;
+
+    cout << "\nBatas bermain hingga jam " << jam_bts << " : " << menit_bts << endl;
+    cout << "Bayar : " << bayar << endl;
 
     return 0;
 }
